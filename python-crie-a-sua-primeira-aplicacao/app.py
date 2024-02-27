21ad1e934bdcc95e68ccad8664bd9cdcb490189b
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-27 14:46:53 -0300

    Primeiro script em python

---
+++ python-crie-a-sua-primeira-aplicacao/app.py
@@ -0,0 +1,16 @@
+"""
+    Aplicação principal
+"""
+
+print("Hello Word!")
+
+print("1 - Cadastrar Restaurente")
+print("2 - Listar Restaurente")
+print("3 - Ativar Restaurente")
+print("4 - Sair do Restaurante")
+
+print('\n')
+
+opcao_esolhida = input("Digte a opção desejada: ")
+
+print(f"A opção desejado é: {opcao_esolhida}")
