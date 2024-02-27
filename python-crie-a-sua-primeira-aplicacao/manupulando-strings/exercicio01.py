96bfeff41c7796358709ad4b0af67757749a9fe7
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-27 15:07:52 -0300

    Exercicio01

---
+++ python-crie-a-sua-primeira-aplicacao/manupulando-strings/exercicio01.py
@@ -0,0 +1,14 @@
+"""
+    Curso: Python: Crie a sua primeira Aplicação
+    Diretório: Manipulação de Strings
+    Arquivo: exercicio01.py
+    Dado: Imprima a frase: Python na Escola de Programação da Alura.
+"""
+
+# Usando um função para realizar esse processo
+def imprimir_mensagem(mensagem):
+    print(mensagem)
+
+
+# Testando o desejado.
+imprimir_mensagem("Python na Escola de Programação da Alura.")
