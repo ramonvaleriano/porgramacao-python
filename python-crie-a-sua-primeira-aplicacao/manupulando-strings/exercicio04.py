21204d9815fe21a9fdfcdc12cec0fac71dd54161
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-27 15:23:15 -0300

    Exercicio04

---
+++ python-crie-a-sua-primeira-aplicacao/manupulando-strings/exercicio04.py
@@ -0,0 +1,12 @@
+"""
+    Curso: Python: Crie a sua primeira Aplicação
+    Diretório: Manipulação de Strings
+    Arquivo: exercicio04.py
+    Dado: Imprima a frase: O valor arredondado de pi é: {pi_arredondado} em que o valor de pi precisa ser
+          armazenado em uma variável e arredondado para apenas duas casas decimais.
+"""
+
+from math import pi
+
+# Fzendo o processo desejado.
+print(f"{pi:.2f}")
