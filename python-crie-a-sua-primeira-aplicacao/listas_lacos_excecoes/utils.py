554b3bf99d61a0ab3eecf13f0e69c140dadc7819
Ramon Valeriano <rrvaleriano@gmail.com>
2024-02-29 08:45:24 -0300

    Não permissão quando já exisitir um restaurante cadastrado

--- python-crie-a-sua-primeira-aplicacao/listas_lacos_excecoes/utils.py
+++ python-crie-a-sua-primeira-aplicacao/listas_lacos_excecoes/utils.py
@@ -17,6 +17,12 @@ def cadastrar_restaurantes(lista_restaurante, restaurante, status):
         lista_restaurante.append(registro_restaurentes)
 
     elif len(lista_restaurante) != 0:
+
+        validacao = existe_dado_desejado(lista_restaurante, restaurante)
+        if validacao:
+            print('Essa restaurante já existe, não é possível cadastrar com o mesmo nome')
+            return lista_restaurante
+
         quantidade_restaurantes = len(lista_restaurante)
         contador = quantidade_restaurantes + 1
 
@@ -29,6 +35,23 @@ def cadastrar_restaurantes(lista_restaurante, restaurante, status):
         lista_restaurante.append(registro_restaurentes)
 
     return lista_restaurante
+
+def existe_dado_desejado(lista_restaurantes, restaurante):
+    if type(lista_restaurantes) != list:
+        print('Não é uma lsita de dados.')
+        return lista_restaurantes
+    
+    if len(lista_restaurantes) == 0:
+        print('Não há restaurentes cadastrados')
+        return lista_restaurantes
+    
+    encontrou = False
+    for dado in listar_restaurantes:
+        if dado['restaurante'].lower() == restaurante.lower():
+            encontrou = True
+            break
+    
+    return encontrou
 
 def listar_restaurantes(lista_restaurantes):
 
@@ -58,4 +81,16 @@ def ativar_restaurente(lista_restaurantes, restaurante, status):
     for dado in listar_restaurantes:
         if dado['restaurante'].lower() == restaurante.lower():
             encontrou = True
+
+            if dado.get('status') == status:
+                print('O status já é o mesmo usado.')
+                break
+
+            elif dado.get('status') != status:
+                dado['status'] = status
+        
+        if encontrou:
+            break
+    
+    return lista_restaurantes
 
