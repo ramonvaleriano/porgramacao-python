9d6c94b1ccdbc031e4ccda176c6fb88928e154ba
Ramon Valeriano <rrvaleriano@gmail.com>
2024-03-01 08:22:35 -0300

    Exercicio03

---
+++ python_crie_a_sua_primeira_aplicacao/listas_lacos_excecoes/Exercicio03.py
@@ -0,0 +1,14 @@
+"""
+    Curso: Python: Crie a sua primeira Aplicação
+    Diretório: Lista, laços e exeções
+    Arquivo: exercicio03.py
+    Dado: Utilize um loop for para calcular a soma dos números ímpares de 1 a 10.
+"""
+
+# Funções para resolver os exercícios:
+def soma_numeros_da_lista(lista_de_dados):
+    soma = 0
+    for dado in lista_de_dados:
+        soma += dado
+
+    return soma
