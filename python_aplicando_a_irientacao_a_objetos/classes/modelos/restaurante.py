793a3e0942fa99b479aace5574a4a0d7701ca10c
Ramon Valeriano <rrvaleriano@gmail.com>
2024-03-04 08:49:53 -0300

    Iniciando Python Aplicando Orientação a Objeto

---
+++ python_aplicando_a_irientacao_a_objetos/classes/modelos/restaurante.py
@@ -0,0 +1,16 @@
+"""
+    Curso: Python: Aplicando a Orientação a Objetos
+    Diretório: classes
+    Arquivo: restaurante.py
+    Dado: 
+"""
+
+class Restaurante:
+
+    def __init__(self) -> None:
+        nome: str = ''
+        categoria: str = ''
+        ativo = False
+
+
+
